11ada7c1c12768ec90a6140432d1898dcb054cac
D0ubl3-A <D0ubl3a0@gmail.com>
2026-07-13 17:34:30 -0700

    Assemble embedded lizard and AI bird images

---
+++ lib/viral-image-data.ts
@@ -0,0 +1,9 @@
+import original0 from "./viral-image-chunks/original-0";
+import original1 from "./viral-image-chunks/original-1";
+import original2 from "./viral-image-chunks/original-2";
+import enhanced0 from "./viral-image-chunks/enhanced-0";
+import enhanced1 from "./viral-image-chunks/enhanced-1";
+
+export const originalLizardImageDataUrl = `data:image/webp;base64,${original0 + original1 + original2}`;
+
+export const aiEnhancedBirdImageDataUrl = `data:image/webp;base64,${enhanced0 + enhanced1}`;
